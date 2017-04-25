73a3b90e34172c12d28f4cb333d0fecef03b16cd
Romain Pouclet <romain.pouclet@gmail.com>
2017-04-25 11:52:55 -0400

    Add postclone script

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cp "${BUDDYBUILD_SECURE_FILES}/configuration.xml" \
-"${BUDDYBUILD_WORKSPACE}/app/src/main/res/values/configuration.xml"
+CONFIGURATION="${BUDDYBUILD_SECURE_FILES}/configuration.xml"
+TARGET_CONFIGURATION="${BUDDYBUILD_WORKSPACE}/app/src/main/res/values/configuration.xml"
+if [[ -f "$CONFIGURATION" ]]; then
+    cp "${CONFIGURATION}" "${TARGET_CONFIGURATION}"
+fi
