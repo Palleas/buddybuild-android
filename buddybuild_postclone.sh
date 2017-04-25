5c8848a8c0da3520a0c945a8997d118dd731dde1
Romain Pouclet <romain.pouclet@gmail.com>
2017-04-25 11:59:23 -0400

    Add dist configuration file

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -4,4 +4,6 @@ CONFIGURATION="${BUDDYBUILD_SECURE_FILES}/configuration.xml"
 TARGET_CONFIGURATION="${BUDDYBUILD_WORKSPACE}/app/src/main/res/values/configuration.xml"
 if [[ -f "$CONFIGURATION" ]]; then
     cp "${CONFIGURATION}" "${TARGET_CONFIGURATION}"
+else
+    cp "${TARGET_CONFIGURATION}.dist" "${TARGET_CONFIGURATION}"
 fi
