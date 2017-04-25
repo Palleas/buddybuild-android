e93424d72447adcb780e18e987274d937eb002ab
Romain Pouclet <romain.pouclet@gmail.com>
2017-04-25 12:01:36 -0400

    Fix path to dist

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CONFIGURATION="${BUDDYBUILD_SECURE_FILES}/configuration.xml"
-TARGET_CONFIGURATION="${BUDDYBUILD_WORKSPACE}/app/src/main/res/values/configuration.xml"
+CONFIGURATION="${BUDDYBUILD_SECURE_FILES}/configurations.xml"
+TARGET_CONFIGURATION="${BUDDYBUILD_WORKSPACE}/app/src/main/res/values/configurations.xml"
 if [[ -f "$CONFIGURATION" ]]; then
     cp "${CONFIGURATION}" "${TARGET_CONFIGURATION}"
 else
