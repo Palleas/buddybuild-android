71815ebb5c924943a1fc06fccb2e5457fe14be42
Romain Pouclet <romain.pouclet@gmail.com>
2017-04-25 13:31:59 -0400

    List secure files

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -2,6 +2,9 @@
 
 CONFIGURATION="${BUDDYBUILD_SECURE_FILES}/configurations.xml"
 TARGET_CONFIGURATION="${BUDDYBUILD_WORKSPACE}/app/src/main/res/values/configurations.xml"
+
+ls -hal "${BUDDYBUILD_SECURE_FILES}"
+
 if [[ -f "$CONFIGURATION" ]]; then
     cp "${CONFIGURATION}" "${TARGET_CONFIGURATION}"
 else
